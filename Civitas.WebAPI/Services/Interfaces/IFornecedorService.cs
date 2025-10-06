039a8be7b075fe09341220e73a65bb7cdab4fd94
Gustavo-sosu <gustavo.porto1052@gmail.com>
2025-10-06 14:59:37 -0300

    :sparkles: feat: Adição de toda a Data, Objects e Services referente a tabela Fornecedor

---
+++ Civitas.WebAPI/Services/Interfaces/IFornecedorService.cs
@@ -0,0 +1,10 @@
+﻿using Civitas.WebAPI.Objects.Dtos.Entities;
+using Civitas.WebAPI.Objects.Models;
+
+namespace Civitas.WebAPI.Services.Interfaces
+{
+    public interface IFornecedorService : IGenericService<Fornecedor, FornecedorDTO>
+    {
+
+    }
+}
