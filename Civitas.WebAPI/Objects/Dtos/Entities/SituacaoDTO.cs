d894ad7d762ad81711482f5b02380e4e3ced88b1
carlosafcandido <candidocarlos149@gmail.com>
2025-10-07 14:20:11 -0300

    :wastebasket: remove: Removendo o arquivo SituacaoDTO.cs

--- Civitas.WebAPI/Objects/Dtos/Entities/SituacaoDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿using Civitas.WebAPI.Objects.Enums;
-
-namespace Civitas.WebAPI.Objects.Dtos.Entities
-{
-    public class SituacaoDTO
-    {
-        public Situacao Situacao { get; set; }
-    }
-}
