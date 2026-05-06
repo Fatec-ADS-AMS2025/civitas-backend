dde026801542649d0ddd0ebc941cbe72e3e375b6
felipebianchini2006 <felipebianchini02@gmail.com>
2026-05-06 10:30:10 -0300

    feat: mapeia campos opcionais via AutoMapper usando helper jsonb

--- Civitas.WebAPI/Objects/Dtos/Mappings/MappingsProfile.cs
+++ Civitas.WebAPI/Objects/Dtos/Mappings/MappingsProfile.cs
@@ -1,12 +1,13 @@
-﻿using AutoMapper;
+using AutoMapper;
 using Civitas.WebAPI.Objects.Dtos.Entities;
 using Civitas.WebAPI.Objects.Models;
+using Civitas.WebAPI.Services.Validation;
+using System.Text.Json;
 
 namespace Civitas.WebAPI.Objects.Dtos.Mappings
 {
     public class MappingsProfile : Profile
     {
-
         public MappingsProfile()
         {
             CreateMap<Usuario, UsuarioDTO>()
@@ -20,11 +21,74 @@ namespace Civitas.WebAPI.Objects.Dtos.Mappings
             CreateMap<Instituicao, InstituicaoDTO>().ReverseMap();
             CreateMap<Fluxo, FluxoDTO>().ReverseMap();
             CreateMap<UnidadeMedida, UnidadeMedidaDTO>().ReverseMap();
-            CreateMap<TipoDespesa, TipoDespesaDTO>().ReverseMap();
+
+            CreateMap<TipoDespesa, TipoDespesaDTO>()
+                .ForMember(dest => dest.CamposOpcionais, opt => opt.MapFrom(src =>
+                    ParseCamposSafely(src.CamposOpcionais)));
+            CreateMap<TipoDespesaDTO, TipoDespesa>()
+                .ForMember(dest => dest.CamposOpcionais, opt => opt.MapFrom(src =>
+                    CamposOpcionaisJsonHelper.SerializeCamposDeclarados(src.CamposOpcionais)));
+
             CreateMap<Orcamento, OrcamentoDTO>().ReverseMap();
-            CreateMap<Despesa, DespesaDTO>().ReverseMap();
+
+            CreateMap<Despesa, DespesaDTO>()
+                .ForMember(dest => dest.ValoresOpcionais, opt => opt.MapFrom(src =>
+                    ParseValoresSafely(src.ValoresOpcionais)));
+            CreateMap<DespesaDTO, Despesa>()
+                .ForMember(dest => dest.ValoresOpcionais, opt => opt.MapFrom(src =>
+                    SerializeValoresOpcionais(src.ValoresOpcionais)));
+
             CreateMap<TipoCodigo, TipoCodigoDTO>().ReverseMap();
             CreateMap<UnidadeConsumidora, UnidadeConsumidoraDTO>().ReverseMap();
+        }
+
+        private static IList<string>? ParseCamposSafely(string? json)
+        {
+            if (string.IsNullOrWhiteSpace(json))
+            {
+                return null;
+            }
+
+            try
+            {
+                return CamposOpcionaisJsonHelper.ParseCamposDeclarados(json).ToList();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private static IDictionary<string, JsonElement>? ParseValoresSafely(string? json)
+        {
+            if (string.IsNullOrWhiteSpace(json))
+            {
+                return null;
+            }
+
+            try
+            {
+                return CamposOpcionaisJsonHelper.ParseValoresPreenchidos(json)
+                    .ToDictionary(kv => kv.Key, kv => kv.Value);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private static string? SerializeValoresOpcionais(IDictionary<string, JsonElement>? valores)
+        {
+            if (valores == null)
+            {
+                return null;
+            }
+
+            IReadOnlyDictionary<string, JsonElement> readOnly =
+                valores as IReadOnlyDictionary<string, JsonElement>
+                ?? new Dictionary<string, JsonElement>(valores);
+
+            return CamposOpcionaisJsonHelper.SerializeValoresPreenchidos(readOnly);
         }
     }
 }
